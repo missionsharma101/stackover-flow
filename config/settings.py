0a281a80dda6fb02ae4625a3ed3389db06863c80
mission sharma <mission.ghimire16@gmail.com>
2022-12-28 21:53:39 +0545

    database

--- config/settings.py
+++ config/settings.py
@@ -79,14 +79,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'stackoverflow',
+#         'USER': 'postgres',
+#         'PASSWORD': 'mission@',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432'
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'stackoverflow',
+        'HOST': 'localhost',
+        'PORT': 5432,
         'USER': 'postgres',
-        'PASSWORD': 'mission@',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'PASSWORD': 'postgres',
     }
 }
 AUTH_USER_MODEL = 'stack.User'
